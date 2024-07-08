1d99f84be7bbf357824eb9598b581b01277ed34e
erfan <erfank20041382@gmail.com>
2024-07-08 20:40:46 +0430

    delete and kth smallest and largest elements

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Treap<Integer> treap=new Treap<>();
+        Treap<Long> treap=new Treap<>();
 
-        treap.insert(4);
-        treap.insert(5);
-        treap.insert(3);
-        treap.insert(6);
-//        treap.add(-5);
-        treap.draw();
+        for (long i=0;i<10;i++){
+            treap.insert(i);
+        }
+
+        System.out.println(treap.max().value);
+//        treap.draw();
+//        treap.search((long)6);
     }
 }
--- Treap.java
+++ Treap.java
@@ -36,6 +36,7 @@ public class Treap<T extends Comparable<T>>  {
     private static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_RED = "\u001B[31m";
     private static final String empty = "  ";
+    private static int count =0;
 
     Treap(){
         this.root=null;
@@ -96,7 +97,59 @@ public class Treap<T extends Comparable<T>>  {
         return root;
     }
 
-    public static TreapNode rightRotate(TreapNode y) {
+    public TreapNode<T> remove(T value) throws InterruptedException {
+//        draw(root, getHeightRec(root), true, value, true);
+        this.root=deleteNode(this.root,value);
+        System.out.println("after remove");
+//        draw();
+//        System.out.println(temp.value.toString());
+        return root;
+    }
+    private TreapNode<T> deleteNode(TreapNode<T> root, T key)
+    {
+        // Base case
+        if (root == null) {
+            return root;
+        }
+
+        // IF KEY IS NOT AT ROOT
+        if (key.compareTo(root.value)<0) {
+            root.leftChild = deleteNode(root.leftChild, key);
+        }
+        else if (key.compareTo(root.value)>0 ) {
+            root.rightChild = deleteNode(root.rightChild, key);
+        }
+
+        // IF KEY IS AT ROOT
+        // If left is null
+        else if (root.leftChild == null) {
+            TreapNode temp = root.rightChild;
+            root = null;
+            root = temp; // Make right child as root
+        }
+
+        // If right is null
+        else if (root.rightChild == null) {
+            TreapNode temp = root.leftChild;
+            root = null;
+            root = temp; // Make left child as root
+        }
+
+        // If key is at root and both left and right are not
+        // null
+        else if (root.leftChild.priority < root.rightChild.priority) {
+            root = leftRotate(root);
+            root.leftChild = deleteNode(root.leftChild, key);
+        }
+        else {
+            root = rightRotate(root);
+            root.rightChild = deleteNode(root.rightChild, key);
+        }
+
+        return root;
+    }
+
+    private static TreapNode rightRotate(TreapNode y) {
         TreapNode x = y.leftChild;
         TreapNode T2 = x.rightChild;
 
@@ -124,42 +177,6 @@ public class Treap<T extends Comparable<T>>  {
 
 
 
-    void printTree() {
-        if (root == null) {
-            return;
-        }
-
-        Queue<TreapNode> queue = new LinkedList<>();
-        queue.add(root);
-
-        while (!queue.isEmpty()) {
-            int levelSize = queue.size();
-            StringBuilder levelStr = new StringBuilder();
-
-            for (int i = 0; i < levelSize; i++) {
-                TreapNode currentNode = queue.poll();
-                System.out.println("height :" + getHeightRec(currentNode));
-                String rightPadding=" ".repeat(getHeightRec(currentNode)*2);
-                levelStr.append(rightPadding)
-                        .append(currentNode.value).append(" ".repeat(3));
-//                System.out.println(levelStr);
-
-                if (currentNode.leftChild != null) {
-                    queue.add(currentNode.leftChild);
-                } else if (currentNode.rightChild != null) {
-                    queue.add(new TreapNode(-1,this.getPriority())); // Placeholder for formatting
-                }
-
-                if (currentNode.rightChild != null) {
-                    queue.add(currentNode.rightChild);
-                } else if (currentNode.leftChild != null) {
-                    queue.add(new TreapNode(-1,this.getPriority())); // Placeholder for formatting
-                }
-            }
-            System.out.println(levelStr.toString());
-        }
-    }
-
     private void draw(TreapNode<T> node, int count, Boolean pause, T coloredvalue, Boolean isDel) throws InterruptedException {
 
         go(node, 0);
@@ -170,8 +187,10 @@ public class Treap<T extends Comparable<T>>  {
             for (int j = 0; j < this.a.get(i).size(); j++) {
                 String col = ANSI_CYAN;
                 if (coloredvalue != null)
-                    if (isDel)
+                    if (isDel){
                         col = (a.get(i).get(j).equals(coloredvalue.toString())) ? ANSI_RED : ANSI_CYAN;
+
+                    }
                     else
                         col = (a.get(i).get(j).equals(coloredvalue.toString())) ? ANSI_YELLOW : ANSI_CYAN;
                 System.out.print(col + a.get(i).get(j) + ANSI_RESET);
@@ -220,7 +239,7 @@ public class Treap<T extends Comparable<T>>  {
 
     public void draw() throws InterruptedException {
         System.out.println("Tree:");
-        draw(root, getHeightRec(root), true, null, true);
+        draw(root, getHeightRec(root), true, null, false);
     }
     public boolean search(T value) throws InterruptedException {
         return search(root, value, true) != null;
@@ -255,6 +274,122 @@ public class Treap<T extends Comparable<T>>  {
             return node;
         }
 
+    }
+
+    public TreapNode<T> findkthSmallest(int k){
+        count=0;
+        return kthSmallest(this.getRoot(),k);
+    }
+
+    private TreapNode<T> kthSmallest(TreapNode<T> root, int k)
+    {
+        // base case
+        if (root == null)
+            return null;
+
+        // search in left subtree
+        TreapNode<T> left = kthSmallest(root.leftChild, k);
+
+        // if k'th smallest is found in left subtree, return it
+        if (left != null)
+            return left;
+
+        // if current element is k'th smallest, return it
+        count++;
+        if (count == k)
+            return root;
+
+        // else search in right subtree
+        return kthSmallest(root.rightChild, k);
+    }
+
+    public TreapNode<T> findkthLargest(int k){
+        count=0;
+        return KthLargestUsingMorrisTraversal(root,k);
+    }
+
+
+    private TreapNode<T> KthLargestUsingMorrisTraversal(TreapNode<T> root, int k)
+    {
+        TreapNode<T> curr = root;
+        TreapNode<T> Klargest = null;
+
+        // count variable to keep count of visited Nodes
+        int count = 0;
+
+        while (curr != null)
+        {
+            // if right child is NULL
+            if (curr.rightChild == null)
+            {
+
+                // first increment count and check if count = k
+                if (++count == k)
+                    Klargest = curr;
+
+                // otherwise move to the left child
+                curr = curr.leftChild;
+            }
+
+            else
+            {
+
+                // find inorder successor of current Node
+                TreapNode<T> succ = curr.rightChild;
+
+                while (succ.leftChild != null && succ.leftChild != curr)
+                    succ = succ.leftChild;
+
+                if (succ.leftChild == null)
+                {
+
+                    // set left child of successor to the
+                    // current Node
+                    succ.leftChild = curr;
+
+                    // move current to its right
+                    curr = curr.rightChild;
+                }
+
+                // restoring the tree back to original binary
+                // search tree removing threaded links
+                else
+                {
+
+                    succ.leftChild = null;
+
+                    if (++count == k)
+                        Klargest = curr;
+
+                    // move current to its left child
+                    curr = curr.leftChild;
+                }
+            }
+        }
+        return Klargest;
+    }
+
+    public TreapNode<T> min(){
+        return getMin(root);
+    }
+
+
+    private TreapNode<T> getMin(TreapNode<T> root){
+        if (root.leftChild==null)
+            return root;
+
+        return getMin(root.leftChild);
+    }
+
+    public TreapNode<T> max(){
+        return getMax(root);
+    }
+
+    private TreapNode<T> getMax(TreapNode<T> root){
+        if (root.rightChild==null)
+            return root;
+
+        return getMax(root.rightChild);
     }
 
 }
